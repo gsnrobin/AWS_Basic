813e37bd101c625f6e056de378dbeffa166ee49b
Golam shah newaz Robi <105287720+gsnrobin@users.noreply.github.com>
2023-03-20 15:33:18 +0600

    Add files via upload

---
+++ Assignment-3.py
@@ -0,0 +1,39 @@
+Q:What is self understanding about cloud computing?
+Cloud computing means using any cloud service using.
+It's not a traditional service as a link we need to set up a server room, AC, electricity, hardware, server, networking, firewalls, routers, modem, switches, and lots of think.
+mainly Cloud computing is a virtualization-based technology that allows us to create, configure, and customize applications via an internet connection. 
+The cloud technology includes a development platform, hard disk, software application, and database.
+
+
+Write Cloud Deloyment Models name ?
+
+There are three cloud deployment models
+ public, private, and hybrid
+
+
+Q-3:Write Cloud Computing models name?
+There are three Cloud computing models ,
+Software as a Service (SaaS), Platform as a Service (PaaS), Infrastructure as a Service (IaaS)
+
+Q-4 :What is full meaning of EC2 & S3:
+Full meaning of EC2 is Elastic Compute Cloud and Full meaning of s3 is simple storage service .
+
+Q-5:How many regions & AZs aws have?
+AWS have 29 regions and AZs have 99 Availability Zones
+
+Q-5:Write the names AWS cores services?
+The names of AWS Cores services is 
+EC2,AWS Elastic Beanstaik,Auto Scaling, Lambda function,AWS batch,Amazon lightsail,AWS elastic container registry,AWS VPC,Route 53,AWS Direct connect,Amazone S3,Amazon EBS,Amazon CloidFront,Amazon Glacier, Amazon Elastic File system,AWS snowball,Storage Gateway,AWS RDS,AWS DynamoDB, AWS Redshift,AWS DB migration service,AWS ElasticCache.
+
+Q-6:When AWS EC2 services first luanch ?
+ AWS EC2 services  first launched in August 2006.
+
+
+Q-7:What is VPC ?
+VPC means Virtual private cloud.
+When a user has a multi-network or service, as link guest network, VIP network (For boss), employ network, VPN network, or databased network for that we need to create a different network,in this cases we use VPC.
+
+
+
+
+
